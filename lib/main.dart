4e6d007527c2cafb8e2b6827f2f6eac930b69824
Ferrozo <ferrozo.c@gmail.com>
2022-10-07 22:17:22 +0100

    creating internal package

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'firebase_options.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+// await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+// );
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
---
+++ packages/authentication_repository/lib/src/auth_repository.dart
@@ -0,0 +1,5 @@
+import 'dart:async';
+
+import 'package:authentication_repository/authentication_repository.dart';
+
+import 'package:cache/cache.dart';
