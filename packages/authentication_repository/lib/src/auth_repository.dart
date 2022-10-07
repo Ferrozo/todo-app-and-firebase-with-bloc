c3ad02f6d7d09face1bfb8d59a94a5f15fa43e36
Ferrozo <ferrozo.c@gmail.com>
2022-10-07 22:22:18 +0100

    data layer

--- packages/authentication_repository/lib/src/auth_repository.dart
+++ packages/authentication_repository/lib/src/auth_repository.dart
@@ -1,5 +1,8 @@
 import 'dart:async';
-
 import 'package:authentication_repository/authentication_repository.dart';
-
-import 'package:cache/cache.dart';
+// import 'package:cache/cache.dart';
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+import 'package:firebase_auth_platform_interface/firebase_auth_platform_interface.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:meta/meta.dart';
