8de347395f64c94da43d496405b161614b411381
Ferrozo <ferrozo.c@gmail.com>
2022-10-13 13:53:58 +0100

    firebase login

--- lib/presentation/screen/main_page/main_page.dart
+++ lib/presentation/screen/main_page/main_page.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:bloc/bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../bloc/bloc/auth_bloc.dart';
+import '../../../bloc/bloc/auth_state.dart';
+import '../sign_in/sign_in.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container());
+    final showCurrentUser = FirebaseAuth.instance.currentUser!;
+    return Scaffold(
+      body: BlocListener<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is UnAuthenticated) {
+            Navigator.of(context).pushAndRemoveUntil(
+              MaterialPageRoute(builder: (context) => SignIn()),
+              (route) => false,
+            );
+          }
+        },
+        child: Center(
+          child: Container(
+              child: Column(
+            children: [
+              Text('Email: ${showCurrentUser.email}'),
+              showCurrentUser.photoURL != null
+                  ? Image.network('${showCurrentUser.photoURL}')
+                  : Container()
+            ],
+          )),
+        ),
+      ),
+    );
   }
 }
