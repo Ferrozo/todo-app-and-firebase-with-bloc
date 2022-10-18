3f0dd3b1a89e683d38066763798704b6aaed1e97
Ferrozo <ferrozo.c@gmail.com>
2022-10-18 22:57:47 +0100

    refactoring the app

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
+// ignore: depend_on_referenced_packages
 import 'package:path_provider/path_provider.dart';
 import 'package:todo_app_with_firebase/presentation/screen/home/home_page.dart';
 // ignore: depend_on_referenced_packages
